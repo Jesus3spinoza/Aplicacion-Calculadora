f8990ab643560771fd7fe512569470b3c5779e74
Jesus Espinoza <jesus4espinoza@outlook.com>
2022-08-12 13:10:00 -0700

    commit on 'main'

--- js/MainFunctions.js
+++ js/MainFunctions.js
@@ -4,13 +4,13 @@ var operacion;
 var OperadorLeido = false;
 
 function init(){
-    console.log("iniciado");
+    //console.log("iniciado");
 }
 
 function addVal(val){
     var Actualtext = (document.getElementById("result").value);
     var lastChar = (Actualtext.split("")[Actualtext.length-1]);
-
+    console.log(val);
     switch (val){
         case "/":
             if(lastChar != null && lastChar!=val){
@@ -152,9 +152,88 @@ function doOperation(val1, val2, operator){
             break;
     }
 }
-var nextinput = 0;
+
+const contenedor = document.querySelector('#operaciones-datos');
+let total = 1;
+
 function saveOperation(value){
-nextinput++;
-campo = '<li id="rut'+nextinput+'">Campo:<input type="text" size="20" id="campo' + nextinput + '" name="campo' + nextinput + '"&nbsp; /></li>';
-$("#campos").append(campo);
+    if(value != ""){
+        let OperadorAlFinal = SaberSiTerminaConUnOperador(value);
+        if(OperadorAlFinal == true){
+           value = value.substring(0, value.length-1);
+           
+        }
+        let div = document.createElement('div');
+        div.innerHTML = `
+        <div class="container subContenedorOperaciones-desing" id="subContenedorOperaciones">
+        <div class="row">
+        <div class="col-1 order-firt"><label id="numeroDato" style="color:white;" class="col order-firt">${total++} </label>
+        </div>
+        <div class="col-6 contenedorInput"><input id="descripcion-operacion" class="DescripcionValorLeido" type="text" placeholder="Descripcion"></div>
+        <div class="col-2"><span id="valorLeido"class="valorLeido "> ${parseInt(value)}</span></div>
+        <div class="col-1 order-last"><button class="btn-delete" onclick="eliminar(this)">Eliminar</button></div>
+        </div>
+        </div>
+`;
+        contenedor.appendChild(div);
+    }
+}
+
+function SaberSiTerminaConUnOperador(val){
+    var lastChar = (val.split("")[val.length-1]);
+    switch(lastChar){
+        case "+":
+            return true;
+            break;
+        case "-":
+            return true;
+            break;   
+        case "/":
+            return true;
+            break;  
+        case "x":
+            return true;
+            break;  
+        case "%":
+            return true;
+            break; 
+        default:
+            return false;
+            break
+    }
+}
+
+/**
+ * Método para eliminar el div contenedor del input
+ * @param {this} e 
+ */
+ const eliminar = (e) => {
+    const divPadre = e.parentNode;
+    contenedor.removeChild(divPadre);
+    actualizarContador();
+};
+
+const actualizarContador = () => {
+    let divs = contenedor.children;
+    total = 1;
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].children[0].innerHTML = total++;
+    }//end for
+};
+
+function LimpiarDatos(){
+    document.getElementById("operaciones-datos").innerHTML="";
+    total = 1;
+}
+
+function ImprimirDatos(numeroDato, descripcion, valorLeido){
+    var numero = document.getElementById(numeroDato);
+    var valor = document.getElementById(valorLeido);
+    var ventimp = window.open(' ', 'popimpr');
+    ventimp.document.write( numero.innerHTML + " - ");
+    ventimp.document.write( descripcion );
+    ventimp.document.write( valor.innerHTML );
+    ventimp.document.close();
+    ventimp.print( );
+    ventimp.close();
 }
